a3a8b321eb0fbc725f172f97fecc3d74fabd4026
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2026-06-17 13:52:20 +0530

    17th June
    Implement a function to check for valid parentheses using a stack.

---
+++ Stacks/Valid_Paranthesis.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0; i<s.length(); i++){
+            char ch= s[i];
+            //opening brackets
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                //closing bracket- ch
+                if(!st.empty()){
+                    char topch= st.top();
+                    if(ch==')' && topch== '(')
+                        st.pop();
+                    else if(ch=='}' && topch== '{')
+                        st.pop();
+                    else if(ch==']' && topch== '[')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
+    }
+};
